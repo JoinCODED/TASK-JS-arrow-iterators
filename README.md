c2c8a2216f94afc63bbc418438159f39bb4ed560
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2021-12-27 08:26:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Arrow Functions & Iteration Methods Task
+
+## Instructions
+
+**Fork** this repository and clone your fork (make sure you clone it into your `development` directory):
---
+++ iterators.js
@@ -0,0 +1,48 @@
+/*************************************
+ * Challenge 1: engineer(array)
+ *
+ * - Accepts an array
+ * - Logs every element of the array with "eng" next to it
+ * eg: engineer(["zainab","omar","zahraa"]) => eng.zainab, eng.omar, eng.zahraa
+ ************************************/
+const engineer = (array) => {
+  // input code here
+};
+
+/*************************************
+ * Challenge 2: toKWD(wallets)
+ *
+ * - Write a function named toKWD
+ * - Accepts an array of wallets
+ *   in USD
+ * - Returns an array of wallets
+ *   in KWD
+ *
+ * The conversion is:
+ *   kwd = usd * 0.30
+ ************************************/
+
+/**************************************
+ * Challenge 3: richestWallet(wallets,maxAmount)
+ *
+ * - Write a function named richestWallet
+ * - Accepts an array of wallets
+ * - Accepts a max amount
+ * - Returns an array of wallets
+ *   that exceed the maxAmount
+ ***************************************/
+
+/******************************************
+ * Challenge 4: makeLiteralGrades(grades)
+ *
+ * - Write a function named makeLiteralGrades
+ * - Accepts an array of grades as numbers
+ * - Converts evrey grade to its currosponding
+ * literal grade in the following scale
+ * 90-100 : A
+ * 80-90  : B
+ * 70-80  : C
+ * 60-70  : D
+ * < 60   : F
+ * Example: makeLiteralGrades([90, 70, 40]) should return ["A", "C", "F"]
+ *******************************************/
